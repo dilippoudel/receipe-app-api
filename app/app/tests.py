0feb56369aceec368589f55c23b8ffce5b9f2219
dilippoudel <dilippoudel578@gmail.com>
2024-09-05 21:57:07 +0300

    Added test file for DB connection

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,12 +3,13 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     """Test the calc model"""
 
     def test_add_numbers(self):
         """Test adding number together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
