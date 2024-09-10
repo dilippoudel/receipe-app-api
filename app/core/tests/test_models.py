ed3dbd693d4d8612b47228e77f49e9845aa355a5
dilippoudel <dilippoudel578@gmail.com>
2024-09-10 21:15:09 +0300

    Added email normalized function

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -23,9 +23,9 @@ class ModelTests(TestCase):
         # Test email is normalized for new users.
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2@EXAMPLE.com', 'test2@example.com'],
-            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
-            ['test4@EXAMPLE.com', 'test4@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
         ]
 
         for email, expected in sample_emails:
