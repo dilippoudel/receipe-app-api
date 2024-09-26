bdd558d95f654691bd8b4e1a06333c0590154bb9
dilippoudel <dilippoudel578@gmail.com>
2024-09-26 21:10:24 +0300

    Added testcase and functionality

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -64,9 +64,13 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, valitade_data):
         """Update recipe."""
         tags = valitade_data.pop('tags', None)
+        ingredients = valitade_data.pop('ingredients', None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in valitade_data.items():
             setattr(instance, attr, value)
